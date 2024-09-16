da969c164133f65f778bd40996155c9d20a9cb97
Deep <133337403+Deep-Axe@users.noreply.github.com>
2024-09-16 10:44:06 +0530

    finding ques

---
+++ OOP Lab/Random Questions/eval1.java
@@ -0,0 +1,56 @@
+//1. WJP to perform the following:
+//Courses(String coursename)
+//Student(String name, int[][] preference , Courses courses[])
+//Create admitCourse()  which admits minimum of 3 courses for each students. Also assign_preferences() to assign preferences to each course.  Use toString() to print the student details and course detail based on highest preference.
+
+
+class Course{
+    
+    
+    public class Student{
+    String name;
+    int[][] preference ;
+    Courses courses[];
+}
+public Student(String name, int preference , String Coursename) {
+    this.name = name;
+    this.preference = preference;
+    this.coursename=coursename;
+}
+void display(){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the name of the student");
+    name=in.nextline();
+    System.out.println("Enter the course name");
+    coursename=in.nextline();
+}
+public String getCoursename() {
+    return coursename;
+}
+public String getName() {
+    return name;
+}
+
+public int getPreference() {
+    return preference;
+}
+public void displayDetails() {
+    System.out.println("Name: " + name);
+    System.out.println("Preference: " + preference);
+    System.out.println("Coursename:"+coursename);
+}
+admitcourse(){
+    if(course[i]<3){
+        print("minimum of three courses required , Try Again");
+    }
+    else{
+        for()
+    }
+    public class Student{
+        public static void main(String[]args){
+            
+        }
+    }
+}
+
+}
