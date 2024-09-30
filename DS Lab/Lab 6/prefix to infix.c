a5809db4d2b7ad51c5f4e050db88271ee307f2c6
Deep <133337403+Deep-Axe@users.noreply.github.com>
2024-09-30 07:29:55 +0530

    Update prefix to infix.c

--- DS Lab/Lab 6/prefix to infix.c
+++ DS Lab/Lab 6/prefix to infix.c
@@ -38,14 +38,15 @@ char* pop(Stack* s) {
     }
 }
 
-void reverseString(char* str) {
+/*void reverseString(char* str) {
     int length = strlen(str);
     for (int i = 0; i < length / 2; i++) {
         char temp = str[i];
         str[i] = str[length - i - 1];
         str[length - i - 1] = temp;
     }
-}
+}*/
+// didn't use reversing here, started from i = len instead in the for loop
 
 
 void prefixToInfix(char* prefix, char* infix) {
