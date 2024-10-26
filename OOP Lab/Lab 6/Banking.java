af57c82eb6151907e6d931830a0b220916e40daa
Deep <133337403+Deep-Axe@users.noreply.github.com>
2024-10-26 15:24:35 +0530

    renaming appropriately

--- OOP Lab/Lab 6/Banking.java
+++ OOP Lab/Lab 6/Banking.java
@@ -76,7 +76,7 @@ class Savings extends Account {
     }
 }
 
-public class Main {
+public class Banking {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
