fb3b9b1191c8bc66904ebf8a937f149485a057c1
Deep <133337403+Deep-Axe@users.noreply.github.com>
2024-11-04 07:31:31 +0530

    Create GraphUsingAdjacencyList.c

---
+++ DS Lab/Lab 12/GraphUsingAdjacencyList.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Graph {
+    int numVertices;
+    struct Node** adjLists;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+    graph->adjLists = (struct Node**)malloc(vertices * sizeof(struct Node*));
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+    }
+    return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest, int directed) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    if (!directed) {
+        newNode = createNode(src);
+        newNode->next = graph->adjLists[dest];
+        graph->adjLists[dest] = newNode;
+    }
+}
+
+void printGraph(struct Graph* graph) {
+    for (int v = 0; v < graph->numVertices; v++) {
+        struct Node* temp = graph->adjLists[v];
+        printf("Vertex %d:", v);
+        while (temp) {
+            printf(" -> %d", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int vertices, edges, directed, src, dest;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &vertices);
+    printf("Enter number of edges: ");
+    scanf("%d", &edges);
+    printf("Enter 1 if directed, 0 if undirected: ");
+    scanf("%d", &directed);
+
+    struct Graph* graph = createGraph(vertices);
+
+    for (int i = 0; i < edges; i++) {
+        printf("Enter edge (source destination): ");
+        scanf("%d %d", &src, &dest);
+        addEdge(graph, src, dest, directed);
+    }
+
+    printGraph(graph);
+
+    return 0;
+}
